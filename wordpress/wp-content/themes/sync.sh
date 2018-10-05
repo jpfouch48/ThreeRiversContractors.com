efce45897fe7e8d432c8cf1f76cce7b201e876a9
Faucher <jon-paul.faucher@ad.dot.gov>
2018-10-05 10:32:28 -0400

    Update sync for only our theme

--- wordpress/wp-content/themes/sync.sh
+++ wordpress/wp-content/themes/sync.sh
@@ -1,1 +1,2 @@
-rsync -urltv --delete -e ssh ./primer/ lorikaycurran@107.180.41.148:/home/lorikaycurran/addon_domains/three_rivers_contractors.com/wp-content/themes/trc/
+rsync -urltv --delete -e ssh ./trc/ lorikaycurran@107.180.41.148:/home/lorikaycurran/addon_domains/three_rivers_contractors.com/wp-content/themes/trc/
+
